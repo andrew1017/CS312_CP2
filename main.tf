b1b05ffb89c874252b2a19a68077830192860f4e
Andrew <haysan@oregonstate.edu>
2025-06-08 17:08:45 -0700

    fix bug in main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 
 provider "aws" { 
   region = "us-east-1"
-  shared_credentials_files = "~/.aws/credentials"
+  shared_credentials_files = ["~/.aws/credentials"]
 }
 
 resource "aws_instance" "app_server" {
